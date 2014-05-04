f64afc9c656bfb6a81af1590594569c47b3989c2
Paulo Pires <taralloqueiroiz@gmail.com>
2014-05-04 17:04:06 -0300

    added a sox :D

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-var speak = require('speakable')
+var Speakable = require('speakable');
+
+// Setup google speech
+var speakable = new Speakable();
+
+speakable.on('speechStart', function() {
+  console.log('onSpeechStart');
+});
+
+speakable.on('speechStop', function() {
+  console.log('onSpeechStop');
+  speakable.recordVoice();
+});
+
+speakable.on('speechReady', function() {
+  console.log('onSpeechReady');
+});
+
+speakable.on('error', function(err) {
+  console.log('onError:');
+  console.log(err);
+  speakable.recordVoice();
+});
+
+speakable.on('speechResult', function(spokenWords) {
+  console.log('onSpeechResult:')
+  console.log(spokenWords);
+});
+
+speakable.recordVoice();
